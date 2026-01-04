0c7460a8bc74e560d71656ed987873eafdde7bc6
Kurnalpi Organics <craig.saunders93@gmail.com>
2026-01-04 18:06:47 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,5 @@
-// Get current cart total from localStorage or set to 0
 let cartTotal = localStorage.getItem('cartTotal') ? parseFloat(localStorage.getItem('cartTotal')) : 0;
 
-// Update the cart total display
 function updateCartDisplay() {
     const cartTotalElement = document.getElementById('cart-total');
     if(cartTotalElement) {
@@ -9,7 +7,6 @@ function updateCartDisplay() {
     }
 }
 
-// Add product to cart
 function addToCart(price) {
     cartTotal += price;
     localStorage.setItem('cartTotal', cartTotal);
@@ -17,6 +14,4 @@ function addToCart(price) {
     alert(`Added to cart! Total: R${cartTotal.toFixed(2)}`);
 }
 
-// Initialize cart display on page load
 window.onload = updateCartDisplay;
-
